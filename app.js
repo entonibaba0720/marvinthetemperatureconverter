017da54a35e87c66c86fc884463bd496fea0bcfc
Hoffmann Ági <entonibaba0720@gmail.com>
2018-04-25 11:54:49 +0200

    simple javascript exercise - temperature converter

---
+++ app.js
@@ -0,0 +1,8 @@
+function marvinTheConverter () {
+	var celsius = prompt('What is the temperature outside, in °C?');
+	var fahrenheit = celsius * 9 / 5 + 32;
+	alert('Nice! Did you know that this is a swell ' + fahrenheit + ' degrees in Fahrenheit?');
+	return;
+}
+
+marvinTheConverter();
